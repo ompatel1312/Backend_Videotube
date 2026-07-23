c1f1087f25fdf83f4d2b32170e1cc16204d0df49
ompatel1312 <patelom1327@gmail.com>
2026-07-23 19:59:53 +0530

    set utils for api error  and response

--- src/index.js
+++ src/index.js
@@ -9,4 +9,12 @@ import connectDB from "./db/index.js";
 
 dotenv.config({path:'./.env'});
 
-connectDB();
+connectDB()
+.then(()=>{
+    app.listen(process.env.PORT,()=>{
+        consoel.log(`Server is running on port ${process.env.PORT}`);
+})})
+.catch((err)=>{
+    console.log(`Error in connecting to MONGODB this : ${err}`);
+    process.exit(1);
+} )  
