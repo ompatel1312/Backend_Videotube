5589c39af0a7d5909aff6b8a8356a785f12e61bc
ompatel1312 <patelom1327@gmail.com>
2026-07-31 18:33:50 +0530

    add routes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,15 @@
 import { Router } from "express";
-import { loginUser, logoutUser, registerUser,refreshAccessToken } from "../controllers/user.controller.js";
+import { loginUser,
+     logoutUser, 
+     registerUser,
+     refreshAccessToken,
+      changeCurrentPassword,
+       getCurrentUser,
+        updateAccountDetail, 
+        updateUserAvatar,
+         updateUserCoverImage, 
+         getUserChannelProfile, 
+         getWattchHistory } from "../controllers/user.controller.js";
 import {upload} from "../middlewares/multer.middleware.js";
 import { uploadImageOnCloudinary } from "../utils/cloudinary.js";
 import { verfiyJWT } from "../middlewares/auth.middleware.js";
@@ -24,5 +34,13 @@ router.route("/login").post(loginUser)
 //secure routr
 router.route("/logout").post(verfiyJWT,logoutUser)
 router.route("/refreh-token").post(refreshAccessToken)
+router.route("/change-password").post(verfiyJWT,changeCurrentPassword)
+router.route("/current-user").get(verfiyJWT,getCurrentUser)
+router.route("/update-account").patch(verfiyJWT,updateAccountDetail)
+router.route("/avatar").patch(verfiyJWT,upload.single("avatar"),updateUserAvatar)
+router.route("/cover-image").patch(verfiyJWT,upload.single("coverImage"),updateUserCoverImage)
+router.route("/c/:username").get(verfiyJWT,getUserChannelProfile)
+router.route("/history").get(verfiyJWT,getWattchHistory)
+
 
 export default router;
